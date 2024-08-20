fe456232e7e728beed4c9cb1ef5c775fea13d825
ailu <im.ailu@outlook.com>
2024-08-20 17:12:52 +0800

    Initial Commit

---
+++ CanvasEditor/CanvasEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasEditorApp.swift
+//  CanvasEditor
+//
+//  Created by Lu Ai on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct CanvasEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
